6d0ce214fb9f8c15a6f8d47f401fc5dd451b4a67
Babiyv <377064910Vo>
2021-07-08 12:14:09 +0300

    Initial commit

---
+++ src/main/java/lesson13/homework/Main.java
@@ -0,0 +1,10 @@
+package lesson13.homework;
+
+/*2.Создать проект Gradle с базовыми конфигурациями на свое усмотрение,
+добавить пару зависимостей(например lombok и на свое усмотрение),
+прописать версию проекта не ниже java 8 и залить на GIT;*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("First gradle project is working");
+    }
+}
